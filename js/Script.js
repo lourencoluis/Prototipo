0d5b3a111d3148a343c0a1c5bdd50d3b28dc2923
luis lourenco <lourencoluisoppx22@gmail.com>
2025-11-10 17:45:29 -0300

    luis home - 10/11
    Co-authored-by: Guilherme Santos <swaryk@users.noreply.github.com>

--- js/Script.js
+++ js/Script.js
@@ -1,4 +1,20 @@
+
 const hamburger = document.querySelector('.hamburguer');
 const nav = document.querySelector('.nav');
 
 hamburger.addEventListener('click', () => nav.classList.toggle("active"));
+
+window.addEventListener('scroll', function(){
+    console.log("passei");
+    const header = document.querySelector(".header");
+    const bannerheight = this.document.querySelector(".banner").offsetHeight;
+
+    if (window.scrollY > bannerheight) { 
+        console.log("passei");
+
+    header.classList.add('scrolled');
+  } else {
+    console.log("passei");
+    header.classList.remove('scrolled');
+  }
+});
